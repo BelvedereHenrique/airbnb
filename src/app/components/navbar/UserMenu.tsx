fca924941ce7c74715ff8189e0e7b6b4b53b6d36
Bruno Belvedere <belvedere.henrique@gmail.com>
2023-06-22 18:36:02 +0100

    Update navbar with dropdown menu

--- src/app/components/navbar/UserMenu.tsx
+++ src/app/components/navbar/UserMenu.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import Image from 'next/image';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { Avatar } from '../Avatar';
+import { MenuItem } from './MenuItem';
 
 export const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = useCallback(() => setIsOpen((value) => !value), []);
   return (
     <div className='relative'>
       <div className='flex flex-row items-center gap-3'>
@@ -15,13 +17,26 @@ export const UserMenu = () => {
         >
           Airbnb your home
         </div>
-        <div className='flex cursor-pointer flex-row items-center gap-3 rounded-full border-[1px] border-neutral-200 p-4 transition hover:shadow-md md:px-2 md:py-1'>
+        <div
+          onClick={toggleOpen}
+          className='flex cursor-pointer flex-row items-center gap-3 rounded-full border-[1px] border-neutral-200 p-4 transition hover:shadow-md md:px-2 md:py-1'
+        >
           <AiOutlineMenu />
           <div className='hidden md:block'>
             <Avatar />
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className='absolute right-0 top-12 w-[40vw] overflow-hidden rounded-xl bg-white text-sm shadow-md md:w-3/4'>
+          <div className='flex cursor-pointer flex-col'>
+            <>
+              <MenuItem onClick={() => {}} label='Login' />
+              <MenuItem onClick={() => {}} label='Sign Up' />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
